657e222553cee5ddea4b18beec731804c50607d5
tutem95 <mateomonsegur@gmail.com>
2024-06-13 19:53:02 -0300

    20

--- appinicio/admin.py
+++ appinicio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from appinicio.models import Auto
+
 
 # Register your models here.
+
+admin.site.register (Auto)
--- appinicio/models.py
+++ appinicio/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Auto(models.Model):
     modelo = models.CharField(max_length=20)
     marca = models.CharField(max_length=20)
+    
+    
+    def __str__(self):
+        return f'Auto {self. marca} {self.modelo}'
